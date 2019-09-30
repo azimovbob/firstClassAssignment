3eb250aa35f8d28ebfe4e5299f17f09731b7140b
bob <bob>
2019-09-29 23:46:56 -0400

    Test Activity

---
+++ app/src/main/java/azimov/example/bobazimov/firstActivity.kt
@@ -0,0 +1,15 @@
+package azimov.example.bobazimov
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.appcompat.app.AppCompatActivity
+
+
+class firstActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firstlayout)
+    }
+}
